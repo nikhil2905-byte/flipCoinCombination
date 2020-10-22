f380de0b2bc22a54f8948a69f3da075245f16dd7
unknown <nikhilnakirikanti27@gmail.com>
2020-10-22 16:54:12 +0530

    DoubletCombination is added

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -11,6 +11,9 @@ function flipCoin(){
    fi
 }
 
+echo "Singlet Combination"
+echo "******************"
+
 declare -A singlets
 declare -A  winingCombination
 isHead="HEAD"
@@ -30,5 +33,34 @@ echo "Generated Singlets :" ${singlets[@]}
 echo "Percentage of Singlets";
 echo "Percentage of Heads :" $((heads*100/n));
 echo "Percentage of Tails :" $((tails*100/n));
-echo "WiningCombination[H] :" $((heads*100/n))
-echo "WiningCombination[T] :" $((tails*100/n))
+
+echo "Doublet Combination"
+echo "=================="
+
+declare -A doublets
+hh=0
+ht=0
+th=0
+tt=0
+for (( i=0;i<n;i++ ))
+do
+	result="$( flipCoin )""$( flipCoin )"
+	doublets[$i]=$result;
+	if [ "$result" == "HEADHEAD" ]
+   then
+		hh=$((hh+1));
+	elif [ "$result" == "HEADTAIL" ]
+	then
+		ht=$((ht+1));
+	elif [ "$result" == "TAILHEAD" ]
+	then
+		th=$((th+1));
+	else
+		tt=$((tt+1));
+	fi
+done
+echo "Generated doublets:" ${doublets[@]};
+echo "Percentage of HH = $((hh*100/n))";
+echo "Percentage of TT = $((tt*100/n))";
+echo "Percentage of HT = $((ht*100/n))";
+echo "Percentage of TH = $((th*100/n))";
