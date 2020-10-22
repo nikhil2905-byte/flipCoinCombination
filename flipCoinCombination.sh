f704a0640e6fbf136b912c906f068f6737a15bda
unknown <nikhilnakirikanti27@gmail.com>
2020-10-22 15:39:50 +0530

    Head or Tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
 echo "Welcome to Flip Coin Combination Problem"
+
+   randomCheck=$((RANDOM%2))
+   if [ $randomCheck -eq 0 ]
+   then
+      echo "HEAD"
+   else
+      echo "TAIL"
+   fi
